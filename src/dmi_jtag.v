d4e60e8f330af6750b3e8ca15c497d3255d239ae
Kung0605 <kungtacheng920605@gmail.com>
2024-05-16 17:18:28 +0800

    update src

--- src/dmi_jtag.v
+++ src/dmi_jtag.v
@@ -1,21 +1,3 @@
-/* Copyright 2018 ETH Zurich and University of Bologna.
-* Copyright and related rights are licensed under the Solderpad Hardware
-* License, Version 0.51 (the “License”); you may not use this file except in
-* compliance with the License.  You may obtain a copy of the License at
-* http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-* or agreed to in writing, software, hardware and materials distributed under
-* this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
-* CONDITIONS OF ANY KIND, either express or implied. See the License for the
-* specific language governing permissions and limitations under the License.
-*
-* File:   axi_riscv_debug_module.sv
-* Author: Florian Zaruba <zarubaf@iis.ee.ethz.ch>
-* Date:   19.7.2018
-*
-* Description: JTAG DMI (debug module interface)
-*
-*/
-
 module dmi_jtag (
   input           clk_i,      // DMI Clock
   input           rst_ni,     // Asynchronous reset active low
